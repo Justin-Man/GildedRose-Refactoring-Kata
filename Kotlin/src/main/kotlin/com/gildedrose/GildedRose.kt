17d9f80d14313d02d06771e7ebbe71f51beba989
Justin Man <justin.man@moonpig.com>
2020-06-13 17:13:53 +0100

    Implement regression tests to capture current system requirements

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,23 +7,24 @@ class GildedRose(var items: Array<Item>) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+                        items[i].quality = items[i].quality - 1 // items degrade in quality value except aged brie, passes and Sulfuras
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+            }
+            else {
+                if (items[i].quality < 50) { // The Quality of an item is never more than 50
+                    items[i].quality = items[i].quality + 1 // Therefore keep increasing quality value
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (items[i].sellIn < 11) { // When there are less than 12 days to sell the pass
+                            if (items[i].quality < 50) { // As long as quality is not over 50
+                                items[i].quality = items[i].quality + 1 // "Backstage passes", increases in Quality as its SellIn value approaches
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
+                        if (items[i].sellIn < 6) { // When there are less than 7 days to sell the item
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                                items[i].quality = items[i].quality + 1 // increase quality if its not more than 50
                             }
                         }
                     }
@@ -31,21 +32,21 @@ class GildedRose(var items: Array<Item>) {
             }
 
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+                items[i].sellIn = items[i].sellIn - 1 // decrease the number of days to sell the item
             }
 
-            if (items[i].sellIn < 0) {
+            if (items[i].sellIn < 0) { // if past the sell by date
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
+                        if (items[i].quality > 0) { // The Quality of an item is never negative
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        items[i].quality = items[i].quality - items[i].quality // Quality of Backstage passes drops to 0 after the concert
                     }
-                } else {
+                } else { // For Aged brie, backstage passes and Sulfuras, increase quality
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,110 @@
 package com.gildedrose
 
-import org.junit.Assert.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+    @Test
+    fun `decrease quality of regular item as sell-in decreases`() {
+        val items = arrayOf(Item("item", 8, 10))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(9)
     }
 
+    @Test
+    fun `should decrease regular item quality twice as fast when sell-in value is passed`() {
+        val items = arrayOf(Item("item", -1, 10))
+        val app = GildedRose(items)
 
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(8)
+    }
+
+    @Test
+    fun `should not decrease item quality when quality is zero`() {
+        val items = arrayOf(Item("item", 8, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(0)
+    }
+
+    @Test
+    fun `increase quality of aged brie when sell-in date decreases`() {
+        val items = arrayOf(Item("Aged Brie", 8, 10))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(11)
+    }
+
+    @Test
+    fun `should not increase item quality more than 50`() {
+        val items = arrayOf(Item("Aged Brie", 8, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(50)
+    }
+
+    @Test
+    fun `should not decrease sulfuras sell-in or quality value`() {
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 8, 80))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        assertThat(items.first().sellIn).isEqualTo(8)
+        assertThat(items.first().quality).isEqualTo(80)
+    }
+
+    @Test
+    fun `increase backstage passes quality by one when sell-in value of more than 10 decreases`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 11, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(21)
+    }
+
+    @Test
+    fun `increase backstage passes quality by two when sell-in value of 10 or less decreases`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(22)
+    }
+
+    @Test
+    fun `increase backstage passes quality by three when sell-in value of 5 or less decreases`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(23)
+    }
+
+    @Test
+    fun `decrease backstage passes quality to zero when sell-in value is less than 0`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertThat(items.first().quality).isEqualTo(0)
+    }
 }
 
 
