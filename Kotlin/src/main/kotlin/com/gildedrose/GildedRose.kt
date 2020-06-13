3c241cc74b96150d05749bf70397aae886ff21c2
Justin Man <justin.man@moonpig.com>
2020-06-13 19:25:25 +0100

    Use post increment and decrement shorthand syntax

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,24 +7,24 @@ class GildedRose(var items: Array<Item>) {
             if (items[i] !is AgedBrie && items[i] !is BackstageConcertPasses) {
                 if (items[i].quality > 0) {
                     if (items[i] !is Sulfuras) {
-                        items[i].quality = items[i].quality - 1 // items degrade in quality value except aged brie, passes and Sulfuras
+                        items[i].quality-- // items degrade in quality value except aged brie, passes and Sulfuras
                     }
                 }
             }
             else {
                 if (items[i].quality < 50) { // The Quality of an item is never more than 50
-                    items[i].quality = items[i].quality + 1 // Therefore keep increasing quality value
+                    items[i].quality++ // Therefore keep increasing quality value
 
                     if (items[i] is BackstageConcertPasses) {
                         if (items[i].sellIn < 11) { // When there are less than 12 days to sell the pass
                             if (items[i].quality < 50) { // As long as quality is not over 50
-                                items[i].quality = items[i].quality + 1 // "Backstage passes", increases in Quality as its SellIn value approaches
+                                items[i].quality++ // "Backstage passes", increases in Quality as its SellIn value approaches
                             }
                         }
 
                         if (items[i].sellIn < 6) { // When there are less than 7 days to sell the item
                             if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1 // increase quality if its not more than 50
+                                items[i].quality++ // increase quality if its not more than 50
                             }
                         }
                     }
@@ -38,16 +38,16 @@ class GildedRose(var items: Array<Item>) {
                     if (items[i] !is BackstageConcertPasses) {
                         if (items[i].quality > 0) { // The Quality of an item is never negative
                             if (items[i] !is Sulfuras) {
-                                items[i].quality = items[i].quality - 1
+                                items[i].quality--
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality // Quality of Backstage passes drops to 0 after the concert
+                        items[i].quality = 0 // Quality of Backstage passes drops to 0 after the concert
                     }
                 }
                 else { // For Aged brie, backstage passes and Sulfuras, increase quality
                     if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                        items[i].quality++
                     }
                 }
             }
