cad2375f8d5bc77b62004b582ae4fe4d9a8ca9f1
Justin Man <justin.man@moonpig.com>
2020-06-13 19:11:57 +0100

    Implement inheritance to replace name matching with type

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,9 +4,9 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (items[i] !is AgedBrie && items[i] !is BackstageConcertPasses) {
                 if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    if (items[i] !is Sulfuras) {
                         items[i].quality = items[i].quality - 1 // items degrade in quality value except aged brie, passes and Sulfuras
                     }
                 }
@@ -15,7 +15,7 @@ class GildedRose(var items: Array<Item>) {
                 if (items[i].quality < 50) { // The Quality of an item is never more than 50
                     items[i].quality = items[i].quality + 1 // Therefore keep increasing quality value
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i] is BackstageConcertPasses) {
                         if (items[i].sellIn < 11) { // When there are less than 12 days to sell the pass
                             if (items[i].quality < 50) { // As long as quality is not over 50
                                 items[i].quality = items[i].quality + 1 // "Backstage passes", increases in Quality as its SellIn value approaches
@@ -34,17 +34,18 @@ class GildedRose(var items: Array<Item>) {
             decreaseSellInValue(items[i])
 
             if (items[i].sellIn < 0) { // if past the sell by date
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i] !is AgedBrie) {
+                    if (items[i] !is BackstageConcertPasses) {
                         if (items[i].quality > 0) { // The Quality of an item is never negative
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            if (items[i] !is Sulfuras) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality // Quality of Backstage passes drops to 0 after the concert
                     }
-                } else { // For Aged brie, backstage passes and Sulfuras, increase quality
+                }
+                else { // For Aged brie, backstage passes and Sulfuras, increase quality
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
@@ -54,9 +55,8 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun decreaseSellInValue(item: Item) {
-        if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn--
-        }
+        if (item is Sulfuras) return
+        item.sellIn--
     }
 
 }
---
+++ Kotlin/src/main/kotlin/com/gildedrose/SpecialItem.kt
@@ -0,0 +1,19 @@
+package com.gildedrose
+
+class AgedBrie(
+    itemName: String = "Aged Brie",
+    itemSellIn: Int,
+    itemQuality: Int
+) : Item(itemName, itemSellIn, itemQuality)
+
+class BackstageConcertPasses(
+        itemName: String = "Backstage passes to a TAFKAL80ETC concert",
+        itemSellIn: Int,
+        itemQuality: Int
+) : Item(itemName, itemSellIn, itemQuality)
+
+class Sulfuras(
+        itemName: String = "Sulfuras, Hand of Ragnaros",
+        itemSellIn: Int,
+        itemQuality: Int
+) : Item(itemName, itemSellIn, itemQuality)
