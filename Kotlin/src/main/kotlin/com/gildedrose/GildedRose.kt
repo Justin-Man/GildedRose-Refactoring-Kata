db1a82a9ed59c8fad51ab4ab883f146ca901f24d
Justin Man <justin.man@moonpig.com>
2020-06-13 22:25:52 +0100

    Express increasing quality for backstage passes more explicitly
    Add construct to increase quality of back stage passes by one instead of relying on the if statement that increases quality for Aged Brie.
    This puts the quality increase logic for backstages passes in one place under the type check so it is clear to read and understand.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,24 +7,29 @@ class GildedRose(var items: Array<Item>) {
             if (item is RegularItem) {
                 if (item.quality > 0) item.quality-- // items degrade in quality value except aged brie, passes and Sulfuras
             }
-            else {
-                if (item.quality < 50) { // The Quality of an item is never more than 50
-                    item.quality++ // Therefore keep increasing quality value
-
-                    if (item is BackstageConcertPasses) {
-                        if (item.sellIn < 11) { // When there are less than 12 days to sell the pass
-                            if (item.quality < 50) { // As long as quality is not over 50
-                                item.quality++ // "Backstage passes", increases in Quality as its SellIn value approaches
-                            }
-                        }
-
-                        if (item.sellIn < 6) { // When there are 5 days or less to sell the pass
-                            if (item.quality < 50) {
-                                item.quality++ // increase quality if its not more than 50
-                            }
-                        }
+            else if (item is BackstageConcertPasses) {
+                if (item.sellIn > 10) { // When there are more than 10 days to sell the pass
+                    if (item.quality < 50) {
+                        item.quality++ // increase quality if its not more than 50
                     }
                 }
+
+                if (item.sellIn < 11) { // When there are 10 or less days to sell the pass
+                        if (item.quality < 50) { // As long as quality is not over 50
+                            item.quality= item.quality + 2 // "Backstage passes", increases in Quality as its SellIn value approaches
+                        }
+                    }
+
+                if (item.sellIn < 6) { // When there are 5 days or less to sell the pass
+                    if (item.quality < 50) {
+                        item.quality++ // increase quality if its not more than 50
+                    }
+                }
+
+
+            } else {
+                if (item.quality < 50)  // The Quality of an item is never more than 50
+                    item.quality++ // Therefore keep increasing quality value
             }
 
             decreaseSellInValue(item)
